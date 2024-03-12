1d198c52ee6513657e177411894c010ed94b72aa
Priyatam Reddy Somagattu <tech.nerd141@gmail.com>
2024-03-11 22:35:19 -0300

    updated port number for app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./Routes/userRoutes.js');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(express.json());
 app.use(router);
